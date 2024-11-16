fcbceb64513966c51ebc65c2716a398b78831c8a
Qzlzdy <qzlzdy@qq.com>
2024-11-16 14:43:15 +0800

    Setu: move local setu library to NAS
    Signed-off-by: Qzlzdy <qzlzdy@qq.com>

--- src/plugins/Setu.py
+++ src/plugins/Setu.py
@@ -5,31 +5,41 @@ require("nonebot_plugin_apscheduler")
 import base64
 import io
 from PIL import Image
+from nmb.NetBIOS import NetBIOS
 from random import randint
+from smb.SMBHandler import SMBHandler
 import sqlite3
+import urllib
 from nonebot import on_command
 from nonebot_plugin_apscheduler import scheduler
 from nonebot.adapters.mirai2.message import MessageSegment
 from nonebot.adapters.mirai2.message import MessageChain
 from ..utils import ASSETS_ROOT
 from ..utils import sendFriendMessage
+from ..config import USERNAME
+from ..config import PASSWORD
+from ..config import HOSTNAME
 
 setu = on_command("来点黄色")
 
 @setu.handle()
-@scheduler.scheduled_job("cron", hour="5-22", minute=50, second=11, id="Setu please")
+@scheduler.scheduled_job("cron", hour="7-22", minute=50, second=11, id="Setu please")
 async def sendSetu():
     db = sqlite3.connect(f"{ASSETS_ROOT}/Setu.db")
     cur = db.cursor()
     sql = "SELECT extension FROM setus WHERE illust_id = ?"
-    for i in range(1):
-        illust_id = randint(79401, 89400)
+    ip_addresses = NetBIOS().queryName(HOSTNAME)
+    for i in range(len(ip_addresses)):
+        illust_id = randint(1, 89400)
         res = cur.execute(sql, (illust_id,))
         ext = res.fetchall()[0][0]
         #infile = open(f"{ASSETS_ROOT}/setu/H{illust_id}.{ext}", "rb").read()
         #buf = base64.b64encode(infile)
-        img = Image.open(f"{ASSETS_ROOT}/setu/H{illust_id}.{ext}")
+        opener = urllib.request.build_opener(SMBHandler)
+        fh = opener.open(f"smb://{USERNAME}:{PASSWORD}@{ip_addresses[0]}/Multimedia/Pictures/setu/H{illust_id}.{ext}")
+        img = Image.open(fh)
         img = img.rotate(180)
+        fh.close()
         buf = io.BytesIO()
         img.save(buf, format="png")
         buf = buf.getvalue()
